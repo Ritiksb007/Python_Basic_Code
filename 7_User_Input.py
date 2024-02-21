a95b9c38a1a4b5bc6f1878ee036613a0837a243f
[Ritiksb007] <[ritikmalviya77m@gmail.com]>
2024-02-22 01:20:40 +0530

    Day 3 of learning python Here I have learnt taking user input

---
+++ 7_User_Input.py
@@ -0,0 +1,12 @@
+a = input("Enter Your Name: ")
+x = input("Enter First Number: ")
+y = input("Enter Second Number: ")
+
+print(a," is a good boy ",a)
+
+print(x+y)
+
+print(int(x)+int(y))
+
+z = input("Enter Your Name: ")
+print(int(x),(z)) #For concatination
