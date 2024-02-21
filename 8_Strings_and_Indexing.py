5a5e14230e9b3b2a7cda5c77c939a781915d5410
[Ritiksb007] <[ritikmalviya77m@gmail.com]>
2024-02-22 01:24:39 +0530

    Day 3 String and Indexing

---
+++ 8_Strings_and_Indexing.py
@@ -0,0 +1,29 @@
+#We can use double or single quotes.
+#cIt just represents string.
+name = "Ritik"
+friend = "Ankush"
+
+print(name)
+print(friend)
+# Make Double Quotes a part of a String 
+String = 'Hii, how are you doing. "I am working with you"'
+print(String)
+
+# For multiline string:
+boy = '''Hello, boy how are you doing.
+Its been for a while.
+You have not meet me.'''
+print(boy)
+
+print("Hello ",name)
+
+#Indexing 
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
+# print(name[5]) This shows error
+print("lets use a for lopo \n")
+for charater in name:
+    print(charater)
